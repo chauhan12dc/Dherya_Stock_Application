4502262b332e40f53e94be75dcb9b3762a26ae4c
chauhan12dc <chauhan12.dc@gmail.com>
2021-09-04 16:15:34 -0400

    Details screen added

--- app/src/main/java/com/example/dherya_stock_application/DetailsActivity.java
+++ app/src/main/java/com/example/dherya_stock_application/DetailsActivity.java
@@ -1,12 +1,13 @@
 package com.example.dherya_stock_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -27,35 +28,44 @@ import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
-
+    String selectedName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details);
 
+        Intent intent = this.getIntent();
+
+        selectedName =intent.getStringExtra("SelectedName");
+        setTitle(selectedName);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 handler.postDelayed(this,10000);
                 MyTask myTask = new MyTask();
                 myTask.execute();
-
             }
         },400);
 
+
     }
-   public class MyTask extends AsyncTask<String,String,String>{
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        finish();
+        return super.onOptionsItemSelected(item);
+    }
 
-       @Override
+    public class MyTask extends AsyncTask<String,String,String> {
+
+        @Override
         protected String doInBackground(String... strings) {
-
             String result = "";
             try {
                 URL url = new URL("https://71iztxw7wh.execute-api.us-east-1.amazonaws.com/interview/favorite-stocks");
@@ -68,11 +78,14 @@ public class MainActivity extends AppCompatActivity {
                     InputStreamReader isw = new InputStreamReader(inputStream);
 
                     int data = isw.read();
+
+
                     while (data != -1) {
                         result += (char) data;
                         data = isw.read();
 
                     }
+
                     // return the data to onPostExecute method
                     return result;
                 }
@@ -91,41 +104,26 @@ public class MainActivity extends AppCompatActivity {
             ArrayList<Stocks> stocksArrayList = new ArrayList<>();
             ListView listView = findViewById(R.id.stock_list);
             try {
-                JSONObject jsonObject = new JSONObject(s);
+                JSONObject jsonObject = new JSONObject(s).getJSONObject(selectedName);
                 Iterator<String> stringIterator = jsonObject.keys();
+                Map<String,String> map = new HashMap<>();
                 while (stringIterator.hasNext())
                 {
-                    String SName = stringIterator.next();
+                    String Key = stringIterator.next();
+                    String Value = jsonObject.getString(Key).toString();
 
-                    Map<String,String> map = new HashMap<String, String>();
-                    map.put("SName",SName);
+                    map.put(Key,Value);
 
-                    JSONObject jsonObject1 = new JSONObject(jsonObject.getJSONObject(SName).toString());
-                    Iterator<String> stringIterator1 = jsonObject1.keys();
-                    while (stringIterator1.hasNext())
-                    {
-                        String Key = stringIterator1.next();
-                        String Value = jsonObject1.getString(Key).toString();
-
-                        map.put(Key,Value);
-
-                    }
-                    Stocks stocks = new Stocks(map.get("SName"),map.get("name"),map.get("price"),map.get("low"),map.get("high"));
-
-                    stocksArrayList.add(stocks);
                 }
-                ListAdapter listAdapter = new ListAdapter(MainActivity.this,stocksArrayList);
-                listView.setAdapter(listAdapter);
+                ((TextView)findViewById(R.id.symbolName)).setText(selectedName);
+                ((TextView)findViewById(R.id.fullName)).setText(map.get("name"));
+                ((TextView)findViewById(R.id.currentPrice)).setText(map.get("price"));
+                ((TextView)findViewById(R.id.lowPrice)).setText(map.get("low"));
+                ((TextView)findViewById(R.id.highPrice)).setText(map.get("high"));
 
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Intent intent = new Intent(MainActivity.this,DetailsActivity.class);
-                        TextView textView = view.findViewById(R.id.Name);
-                       intent.putExtra("SelectedName",textView.getText());
-                       startActivity(intent);
-                    }
-                });
+
+
+
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -133,4 +131,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
